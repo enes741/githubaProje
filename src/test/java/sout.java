cadab83c40fcdc80d2f5c90a970e3a4622e6f45d
enes741 <122235809+enes741@users.noreply.github.com>
2023-06-22 18:02:57 +0300

    Update sout.java

--- src/test/java/sout.java
+++ src/test/java/sout.java
@@ -2,5 +2,6 @@ public class sout {
     public static void main(String[] args) {
         System.out.println("githuba yollama ");
         //localde degisiklilik yaptim
+        //remoteda degisiklilik yaptim
     }
 }
